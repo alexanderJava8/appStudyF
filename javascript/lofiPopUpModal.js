b5e54ab3408579a368b9c53bc95286816174558c
name <name>
2024-02-21 16:58:08 -0600

    feature/pop-up funcionando

---
+++ javascript/lofiPopUpModal.js
@@ -0,0 +1,22 @@
+const openModalBoton = document.getElementById("open-modal")
+const modalWindowOverlay = document.getElementById("modal-overlay")
+const botonCerrarModal = document.getElementById("close-modal")
+
+const showModalWindow = () => {
+    modalWindowOverlay.style.display = 'flex'
+}
+
+const hideModalWindow = () => {
+    modalWindowOverlay.style.display = 'none'
+}
+
+openModalBoton.addEventListener("click", showModalWindow)
+botonCerrarModal.addEventListener("click", hideModalWindow)
+
+const hideModalWindowOnBlur = (evento) => {
+   if(evento.target == evento.currentTarget) {
+       hideModalWindow()
+   }
+}
+
+modalWindowOverlay.addEventListener("click", hideModalWindowOnBlur)
